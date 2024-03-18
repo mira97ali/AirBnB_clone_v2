8862b89fa59d29d3fd265b62c5c1fbd809c0dd0b
Mira Ali <benamara97amira@gmail.com>
2024-03-18 21:45:15 +0300

    inherit from Base

--- models/user.py
+++ models/user.py
@@ -2,10 +2,10 @@
 """This module defines a class User"""
 import sqlalchemy
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
