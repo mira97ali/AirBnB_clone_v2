4a33aebdb482897ed1f6204dd2f1150ed104985c
Mira Ali <benamara97amira@gmail.com>
2024-04-05 00:35:27 +0300

    fix imports

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """Deploy archive!"""
 import os
 
-import fabric.operations
+import fabric.api
 
 
 os.env.hosts = ["100.26.254.240", "52.23.222.226"]
@@ -16,18 +16,18 @@ def do_deploy(archive_path):
     try:
         new_comp = archive_path.split("/")[-1]
         new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
-        fabric.operations.put(archive_path, "/tmp/")
-        fabric.operations.run("sudo mkdir -p {}".format(new_folder))
-        fabric.operations.run(
+        fabric.api.put(archive_path, "/tmp/")
+        fabric.api.run("sudo mkdir -p {}".format(new_folder))
+        fabric.api.run(
             "sudo tar -xzf /tmp/{} -C {}".format(new_comp, new_folder)
         )
-        fabric.operations.run("sudo rm /tmp/{}".format(new_comp))
-        fabric.operations.run(
+        fabric.api.run("sudo rm /tmp/{}".format(new_comp))
+        fabric.api.run(
             "sudo mv {}/web_static/* {}/".format(new_folder, new_folder)
         )
-        fabric.operations.run("sudo rm -rf {}/web_static".format(new_folder))
-        fabric.operations.run("sudo rm -rf /data/web_static/current")
-        fabric.operations.run(
+        fabric.api.run("sudo rm -rf {}/web_static".format(new_folder))
+        fabric.api.run("sudo rm -rf /data/web_static/current")
+        fabric.api.run(
             "sudo ln -s {} /data/web_static/current".format(new_folder)
         )
         return True
