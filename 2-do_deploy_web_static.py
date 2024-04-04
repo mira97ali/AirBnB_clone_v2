dd2da5428b45485288fa91b147a858279b356ab4
Mira Ali <benamara97amira@gmail.com>
2024-04-05 00:20:10 +0300

    fix style

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,13 +18,18 @@ def do_deploy(archive_path):
         new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
         fabric.operations.put(archive_path, "/tmp/")
         fabric.operations.run("sudo mkdir -p {}".format(new_folder))
-        fabric.operations.run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
+        fabric.operations.run(
+            "sudo tar -xzf /tmp/{} -C {}".format(new_comp, new_folder)
+        )
         fabric.operations.run("sudo rm /tmp/{}".format(new_comp))
-        fabric.operations.run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        fabric.operations.run(
+            "sudo mv {}/web_static/* {}/".format(new_folder, new_folder)
+        )
         fabric.operations.run("sudo rm -rf {}/web_static".format(new_folder))
-        fabric.operations.run('sudo rm -rf /data/web_static/current')
-        fabric.operations.run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        fabric.operations.run("sudo rm -rf /data/web_static/current")
+        fabric.operations.run(
+            "sudo ln -s {} /data/web_static/current".format(new_folder)
+        )
         return True
-    except:
+    except Exception as _:
         return False
