ed225f18219f9146a19a85dc5000122a0dcd8116
mira97ali <140742998+mira97ali@users.noreply.github.com>
2024-04-20 23:54:35 +0300

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""List of states"""
+
+from flask import Flask, render_template
+
+from models import *
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """List the states"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Cleanup the server"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
