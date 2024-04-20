e344c477b0587776a6ae6431a2862b6a785281c9
mira97ali <140742998+mira97ali@users.noreply.github.com>
2024-04-21 00:15:05 +0300

    9. Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Cities by states"""
+
+from flask import Flask, render_template
+
+from models import *
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """List cities by states"""
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Cleanup the server"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
