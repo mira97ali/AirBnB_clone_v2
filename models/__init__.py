a308d25853aed639d7c55326464c8a220082827c
mira97ali <140742998+mira97ali@users.noreply.github.com>
2024-04-21 00:06:36 +0300

    test solution

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,12 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 
 
-if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+DB_STORAGE = "db"
+
+storage_type = os.environ.get("HBNB_TYPE_STORAGE")
+
+
+if storage_type == DB_STORAGE:
     storage = DBStorage()
 else:
     storage = FileStorage()
